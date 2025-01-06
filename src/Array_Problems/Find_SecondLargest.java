72e1ff44dd32a4e5a1d0ebeb5dfbfc864630cba9
Vaibhav Turale <vaibhav.turale@g99.plus>
2025-01-06 18:17:08 +0530

    initial commit

--- src/Array_Problems/Find_SecondLargest.java
+++ src/Array_Problems/Find_SecondLargest.java
@@ -7,24 +7,21 @@ public class Find_SecondLargest {
     public static void main(String[] args) {
         int[] arr={5,6,3,2,8,7,9,11,40,33};
         sortArray(arr);
-        findSecondHighest(arr);
     }
 
     static void sortArray(int[] arr){
 
-        for (int endIndex= arr.length-1; endIndex>=0; endIndex--){
-            for (int i=0; i<endIndex; i++){
-                if (arr[i] > arr[i+1]){
-                    int temp= arr[i];
-                    arr[i]= arr[i+1];
-                    arr[i+1]= temp;
+        int temp=0;
+        for (int i= 0; i<=arr.length-1; i++){
+            for (int j=0; j<=arr.length-1; j++){
+                if (arr[i] > arr[j]){
+                    temp= arr[i];
+                    arr[i]= arr[j];
+                    arr[j]= temp;
                 }
             }
         }
-    }
-    static void findSecondHighest(int[] arr){
-        System.out.println("Second Largest Element: "+ arr[arr.length-2]);
-    }
 
-
+        System.out.println("Second Largest Element: "+ arr[1]);
+    }
 }
