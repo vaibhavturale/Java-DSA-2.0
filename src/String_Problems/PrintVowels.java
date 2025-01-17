5c069338ba52cff579f892ad88bbc879c3f6b262
Vaibhav Turale <vaibhav.turale@g99.plus>
2025-01-17 20:23:15 +0530

    initial commit

--- src/String_Problems/PrintVowels.java
+++ src/String_Problems/PrintVowels.java
@@ -1,12 +1,20 @@
 package String_Problems;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 //import java.lang.*;
 public class PrintVowels {
 
     public static void main(String[] args) {
-        String str = "abgsgvennjibnbo";
+        String str = "abedia";
+        printVowelsbyStringBuffer(str);
+        printVowels(str);
+    }
 
+    static void printVowelsbyStringBuffer(String str){
         StringBuilder vowels = new StringBuilder();
-
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
@@ -14,6 +22,30 @@ public class PrintVowels {
                 vowels.append(ch); // Append the vowel to the StringBuilder
             }
         }
-        System.out.println(vowels);
+        System.out.println("Print Vowels by StringBuffer:  "+vowels);
     }
+
+//    ---------------------------------------------------------------
+
+    static void printVowels(String s){
+
+        Set<Character> vowels= new HashSet<>();
+        for (int i=0; i<s.length(); i++){
+            Character ch= s.charAt(i);
+            if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u'){
+                if(vowels.contains(ch)){
+                    vowels.add(ch);
+                }else{
+                    vowels.add(ch);
+                }
+            }
+        }
+        System.out.println(vowels);
+
+        Iterator itr= vowels.iterator();
+        while(itr.hasNext()){
+            System.out.print(itr.next()+" ");
+        }
+    }
+
 }
